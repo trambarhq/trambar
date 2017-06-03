9ea10c13810d72e04e2a5e2557ca1657104c6f39
Chung Leong <chernyshevsky@hotmail.com>
2017-06-03 13:44:00 +0200

    Added stored proc for invalidating rating of candidate story in listing

--- backend/src/lib/stored-procs/functions.js
+++ backend/src/lib/stored-procs/functions.js
@@ -9,3 +9,34 @@ exports.matchAny = function(filters, objects) {
 exports.matchAny.args = 'filters jsonb, objects jsonb[]';
 exports.matchAny.ret = 'boolean';
 exports.matchAny.flags = 'IMMUTABLE';
+
+exports.hasCandidate = function(details, storyId) {
+    var candidates = details.candidates;
+    if (candidates instanceof Array) {
+        for (var i = 0; i < candidates.length; i++) {
+            if (candidates[i].id === storyId) {
+                return true;
+            }
+        }
+    }
+    return false;
+};
+exports.hasCandidate.args = 'details jsonb, storyId int';
+exports.hasCandidate.ret = 'boolean';
+exports.hasCandidate.flags = 'IMMUTABLE';
+
+exports.invalidateCandidate = function(details, storyId) {
+    var candidates = details.candidates;
+    if (candidates instanceof Array) {
+        for (var i = 0; i < candidates.length; i++) {
+            if (candidates[i].id === storyId) {
+                candidates[i].dirty = true;
+                break;
+            }
+        }
+    }
+    return details;
+};
+exports.hasCandidate.args = 'details jsonb, storyId int';
+exports.hasCandidate.ret = 'jsonb';
+exports.hasCandidate.flags = 'IMMUTABLE';
