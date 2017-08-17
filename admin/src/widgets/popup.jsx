59a378ead5f25fa4a60db9048dd09ccb9e37b316
Chung Leong <chernyshevsky@hotmail.com>
2017-08-17 13:21:06 +0200

    Added popup component

---
+++ admin/src/widgets/popup.jsx
@@ -0,0 +1,83 @@
+var _ = require('lodash');
+var React = require('react'), PropTypes = React.PropTypes;
+
+require('./popup.scss');
+
+module.exports = React.createClass({
+    displayName: 'Popup',
+
+    getInitialState: function() {
+        return {
+            open: false,
+        };
+    },
+
+    render: function() {
+        return (
+            <span ref="container" className="popup">
+                {this.renderLabel()}
+                {this.renderWindow()}
+            </span>
+        );
+    },
+
+    renderLabel: function() {
+        var inline = this.findElement('inline');
+        return (
+            <span onClick={this.handleLabelClick}>
+                {inline.props.children}
+            </span>
+        );
+    },
+
+    renderWindow: function() {
+        if (!this.state.open) {
+            return null;
+        }
+        var window = this.findElement('window');
+        return (
+            <div className="window">
+                {window.props.children}
+            </div>
+        );
+    },
+
+    componentDidUpdate: function(prevProps, prevState) {
+        if (prevState.open !== this.state.open) {
+            if (this.state.open) {
+                document.addEventListener('mousedown', this.handleMouseDown);
+            } else {
+                document.removeEventListener('mousedown', this.handleMouseDown);
+            }
+        }
+    },
+
+    componentWillUnmount: function() {
+        if (this.state.open) {
+            document.removeEventListener('mousedown', this.handleMouseDown);
+        }
+    },
+
+    findElement: function(tagName) {
+        var children = React.Children.toArray(this.props.children);
+        return _.find(children, { type: tagName });
+    },
+
+    handleLabelClick: function(evt) {
+        this.setState({ open: !this.state.open });
+    },
+
+    handleMouseDown: function(evt) {
+        var inside = false;
+        var container = this.refs.container;
+        for (var n = evt.target; n; n = n.parentNode) {
+            if (n === container) {
+                inside = true;
+                break;
+            }
+        }
+        if (!inside) {
+            this.setState({ open: false });
+        }
+    },
+});
