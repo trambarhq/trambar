cddb6f862523aee7c270f37711e0d8c80734424c
Chung Leong <chernyshevsky@hotmail.com>
2017-08-28 00:01:14 +0200

    Reworked nav tree.

--- admin/src/widgets/navigation-tree.jsx
+++ admin/src/widgets/navigation-tree.jsx
@@ -1,3 +1,5 @@
+var _ = require('lodash');
+var Promise = require('bluebird');
 var React = require('react'), PropTypes = React.PropTypes;
 var Relaks = require('relaks');
 
@@ -26,6 +28,8 @@ module.exports = Relaks.createClass({
             project: null,
             user: null,
             role: null,
+            repo: null,
+            robot: null,
             server: null,
 
             database: this.props.database,
@@ -36,47 +40,16 @@ module.exports = Relaks.createClass({
         meanwhile.show(<NavigationTreeSync {...props} />);
         return db.start().then((currentUserId) => {
             var params = this.props.route.parameters;
-            if (params.projectId) {
-                var projectId = parseInt(params.projectId);
-                if (projectId) {
-                    var criteria = {
-                        id: projectId
-                    };
-                    return db.findOne({ table: 'project', criteria }).then((project) => {
-                        props.project = project;
+            var objectTypes = _.keys(_.pickBy(props, _.isNull));
+            return Promise.map(objectTypes, (type) => {
+                var id = parseInt(params[type + 'Id']);
+                if (id) {
+                    var criteria = { id };
+                    return db.findOne({ table: type, criteria }).then((object) => {
+                        props[type] = object;
                     });
                 }
-            } else if (params.userId) {
-                var userId = parseInt(params.userId);
-                if (userId) {
-                    var criteria = {
-                        id: parseInt(params.userId)
-                    };
-                    return db.findOne({ table: 'user', criteria }).then((user) => {
-                        props.user = user;
-                    });
-                }
-            } else if (params.roleId) {
-                var roleId = parseInt(params.roleId);
-                if (roleId) {
-                    var criteria = {
-                        id: roleId
-                    };
-                    return db.findOne({ table: 'role', criteria }).then((role) => {
-                        props.role = role;
-                    });
-                }
-            } else if (params.serverId) {
-                var serverId = parseInt(params.serverId);
-                if (serverId) {
-                    var criteria = {
-                        id: serverId
-                    };
-                    return db.findOne({ table: 'server', criteria }).then((server) => {
-                        props.server = server;
-                    });
-                }
-            }
+            });
         }).then(() => {
             return <NavigationTreeSync {...props} />;
         });
@@ -89,6 +62,8 @@ var NavigationTreeSync = module.exports.Sync = React.createClass({
         project: PropTypes.object,
         user: PropTypes.object,
         role: PropTypes.object,
+        repo: PropTypes.object,
+        robot: PropTypes.object,
         server: PropTypes.object,
 
         route: PropTypes.instanceOf(Route).isRequired,
@@ -105,6 +80,8 @@ var NavigationTreeSync = module.exports.Sync = React.createClass({
             project: this.props.project || emptyObject,
             user: this.props.user || emptyObject,
             role: this.props.role || emptyObject,
+            repo: this.props.repo || emptyObject,
+            robot: this.props.robot || emptyObject,
             server: this.props.server || emptyObject,
         };
     },
@@ -116,32 +93,56 @@ var NavigationTreeSync = module.exports.Sync = React.createClass({
      * @param  {Object} nextProps
      */
     componentWillReceiveProps: function(nextProps) {
-        if (nextProps.project) {
-            this.setState({ project: nextProps.project });
-        } else if (nextProps.user) {
-            this.setState({ user: nextProps.user });
-        } else if (nextProps.role) {
-            this.setState({ role: nextProps.role });
-        } else if (nextProps.server) {
-            this.setState({ server: nextProps.server });
+        var keys = _.keys(this.state);
+        var objects = _.pickBy(_.pick(nextProps, keys), Boolean);
+        if (!_.isEmpty(objects)) {
+            this.setState(objects);
         }
     },
 
     render: function() {
+        var rootNodes = this.getRootNodes();
         return (
             <div ref="container" className="navigation-tree">
-                {this.renderProjectSection()}
-                {this.renderUserSection()}
-                {this.renderRoleSection()}
-                {this.renderServerSection()}
-                {this.renderSettingsSection()}
+                {_.map(rootNodes, this.renderRootNode)}
                 {this.renderArrow()}
             </div>
         );
     },
 
+    renderRootNode: function(node, key) {
+        return this.renderNode(node, 1, key);
+    },
+
+    renderNode: function(node, level, key) {
+        var ref;
+        if (this.isActive(node)) {
+            ref = 'activeLink';
+        }
+        var subtree;
+        return (
+            <div className={`level${level}`}>
+                <a ref={ref} href={node.url}>{node.label}</a>
+                {this.renderChildNodes(node, level + 1)}
+            </div>
+        );
+    },
+
+    renderChildNodes: function(node, level) {
+        if (_.isEmpty(node.children)) {
+            return null;
+        }
+        var props = {
+            open: node.showChildren || this.hasActiveChild(node),
+            children: _.map(node.children, (childNode, i) => {
+                return this.renderNode(childNode, level, i)
+            })
+        }
+        return <CollapsibleContainer {...props} />;
+    },
+
     renderArrow: function() {
-        var numbers = [ 'zero', 'one', 'two', 'three' ];
+        var numbers = [ 'zero', 'one', 'two', 'three', 'four' ];
         var arrowProps = {
             ref: 'arrow',
             className: `arrow ${numbers[this.state.arrowCount]} ${this.state.arrowAction}`,
@@ -152,125 +153,8 @@ var NavigationTreeSync = module.exports.Sync = React.createClass({
                 <i className="fa fa-play first" />
                 <i className="fa fa-play second" />
                 <i className="fa fa-play third" />
+                <i className="fa fa-play fourth" />
             </div>
-        );
-    },
-
-    renderProjectSection: function() {
-        var t = this.props.locale.translate;
-        var p = this.props.locale.pick;
-        var project = this.state.project;
-        var projectName = p(project.details.title) || project.name || t('nav-project-name-pending');
-        var projectId = this.props.route.parameters.projectId;
-        var url = this.props.route.path;
-        var listUrl = require('pages/project-list-page').getUrl();
-        var summaryUrl = require('pages/project-summary-page').getUrl({ projectId });
-        var memberListUrl = require('pages/member-list-page').getUrl({ projectId });
-        var repoListUrl = require('pages/repo-list-page').getUrl({ projectId });
-        var robotListUrl = require('pages/robot-list-page').getUrl({ projectId });
-        var openLevel3 = (url === memberListUrl || url === repoListUrl || url === robotListUrl);
-        var openLevel2 = (openLevel3 || url === summaryUrl);
-        return (
-            <div className="level1">
-                {this.renderLink(listUrl, t('nav-projects'))}
-                <CollapsibleContainer open={openLevel2}>
-                    <div className="level2">
-                        {this.renderLink(summaryUrl, projectName)}
-                        <div className="level3">
-                            {this.renderLink(memberListUrl, t('nav-members'))}
-                        </div>
-                        <div className="level3">
-                            {this.renderLink(repoListUrl, t('nav-repositories'))}
-                        </div>
-                        <div className="level3">
-                            {this.renderLink(robotListUrl, t('nav-robots'))}
-                        </div>
-                    </div>
-                </CollapsibleContainer>
-            </div>
-        );
-    },
-
-    renderUserSection: function() {
-        var t = this.props.locale.translate;
-        var userName = _.get(this.state.user, 'details.name') || t('nav-user-name-pending');
-        var userId = this.props.route.parameters.userId;
-        var url = this.props.route.path;
-        var listUrl = require('pages/user-list-page').getUrl();
-        var summaryUrl = require('pages/user-summary-page').getUrl({ userId });
-        var openLevel3 = false;
-        var openLevel2 = (openLevel3 || url === summaryUrl);
-        return (
-            <div className="level1">
-                {this.renderLink(listUrl, t('nav-users'))}
-                <CollapsibleContainer open={openLevel2}>
-                    <div className="level2">
-                        {this.renderLink(summaryUrl, userName)}
-                    </div>
-                </CollapsibleContainer>
-            </div>
-        );
-    },
-
-    renderRoleSection: function() {
-        var t = this.props.locale.translate;
-        var p = this.props.locale.pick;
-        var role = this.state.role;
-        var roleName = p(role.details.title) || role.name || t('nav-role-name-pending');
-        var roleId = this.props.route.parameters.roleId;
-        var url = this.props.route.path;
-        var listUrl = require('pages/role-list-page').getUrl();
-        var summaryUrl = require('pages/role-summary-page').getUrl({ roleId });
-        var openLevel3 = false;
-        var openLevel2 = (openLevel3 || url === summaryUrl);
-        return (
-            <div className="level1">
-                {this.renderLink(listUrl, t('nav-roles'))}
-                <CollapsibleContainer open={openLevel2}>
-                    <div className="level2">
-                        {this.renderLink(summaryUrl, roleName)}
-                    </div>
-                </CollapsibleContainer>
-            </div>
-        );
-    },
-
-    renderServerSection: function() {
-        var t = this.props.locale.translate;
-        var p = this.props.locale.pick;
-        var serverName = p(_.get(this.state.server, 'details.title')) || t('nav-server-name-pending');
-        var serverId = this.props.route.parameters.serverId;
-        var url = this.props.route.path;
-        var listUrl = require('pages/server-list-page').getUrl();
-        var summaryUrl = require('pages/server-summary-page').getUrl({ serverId });
-        var openLevel3 = false;
-        var openLevel2 = (openLevel3 || url === summaryUrl);
-        return (
-            <div className="level1">
-                {this.renderLink(listUrl, t('nav-servers'))}
-                <CollapsibleContainer open={openLevel2}>
-                    <div className="level2">
-                        {this.renderLink(summaryUrl, serverName)}
-                    </div>
-                </CollapsibleContainer>
-            </div>
-        );
-    },
-
-    renderSettingsSection: function() {
-        var t = this.props.locale.translate;
-        var listUrl = require('pages/settings-page').getUrl();
-        return (
-            <div className="level1">
-                {this.renderLink(listUrl, t('nav-settings'))}
-            </div>
-        );
-    },
-
-    renderLink: function(url, label) {
-        var ref = (url === this.props.route.path) ? 'activeLink' : undefined;
-        return (
-            <a ref={ref} href={url}>{label}</a>
         );
     },
 
@@ -284,6 +168,182 @@ var NavigationTreeSync = module.exports.Sync = React.createClass({
         if (prevProps.route !== this.props.route) {
             this.repositionArrow();
         }
+    },
+
+    isActive: function(node) {
+        return node.url === this.props.route.path;
+    },
+
+    hasActiveChild: function(node) {
+        return _.some(node.children, (node) => {
+            return this.isActive(node) || this.hasActiveChild(node);
+        });
+    },
+
+    getRootNodes: function() {
+        return [
+            this.getProjectListNode(),
+            this.getUserListNode(),
+            this.getRoleListNode(),
+            this.getServerListNode(),
+            this.getSettingsNode(),
+        ]
+    },
+
+    getProjectListNode: function() {
+        var t = this.props.locale.translate;
+        var label = t('nav-projects');
+        var url = require('pages/project-list-page').getUrl();
+        var children = [
+            this.getProjectNode(),
+        ];
+        return { label, url, children };
+    },
+
+    getProjectNode: function() {
+        var t = this.props.locale.translate;
+        var p = this.props.locale.pick;
+        var project = this.state.project;
+        var projectId = this.props.route.parameters.projectId;
+        var label = p(project.details.title) || project.name || t('nav-project-name-pending');
+        var url = require('pages/project-summary-page').getUrl({ projectId });
+        var children = [
+            this.getMemberListNode(),
+            this.getRepoListNode(),
+            this.getRobotListNode(),
+        ];
+        return { label, url, children, showChildren: true };
+    },
+
+    getMemberListNode: function() {
+        var t = this.props.locale.translate;
+        var label = t('nav-members');
+        var projectId = this.props.route.parameters.projectId;
+        var url = require('pages/member-list-page').getUrl({ projectId });
+        var children = [
+            this.getMemberNode(),
+        ];
+        return { label, url, children };
+    },
+
+    getMemberNode: function() {
+        var t = this.props.locale.translate;
+        var user = this.state.user;
+        var userId = this.props.route.parameters.userId;
+        var projectId = this.props.route.parameters.projectId;
+        var label = user.details.name || user.username || t('nav-user-name-pending');
+        var url = require('pages/user-summary-page').getUrl({ projectId, userId });
+        return { label, url };
+    },
+
+    getRepoListNode: function() {
+        var t = this.props.locale.translate;
+        var label = t('nav-repositories');
+        var projectId = this.props.route.parameters.projectId;
+        var url = require('pages/repo-list-page').getUrl({ projectId });
+        var children = [
+            this.getRepoNode(),
+        ];
+        return { label, url, children };
+    },
+
+    getRepoNode: function() {
+        var t = this.props.locale.translate;
+        var p = this.props.locale.pick;
+        var repo = this.state.repo;
+        var repoId = this.props.route.parameters.repoId;
+        var projectId = this.props.route.parameters.projectId;
+        var label = p(repo.details.title) || repo.name || t('nav-repo-name-pending');
+        var url = require('pages/repo-summary-page').getUrl({ projectId, repoId });
+        return { label, url };
+    },
+
+    getRobotListNode: function() {
+        var t = this.props.locale.translate;
+        var label = t('nav-robots');
+        var projectId = this.props.route.parameters.projectId;
+        var url = require('pages/robot-list-page').getUrl({ projectId });
+        var children = [
+            this.getRobotNode(),
+        ];
+        return { label, url, children };
+    },
+
+    getRobotNode: function() {
+        var t = this.props.locale.translate;
+        var p = this.props.locale.pick;
+        var robot = this.state.robot;
+        var robotId = this.props.route.parameters.robotId;
+        var projectId = this.props.route.parameters.projectId;
+        var label = p(robot.details.title) || robot.name || t('nav-robot-name-pending');
+        var url = require('pages/robot-summary-page').getUrl({ projectId, robotId });
+        return { label, url };
+    },
+
+    getUserListNode: function() {
+        var t = this.props.locale.translate;
+        var url = require('pages/user-list-page').getUrl();
+        var label = t('nav-users');
+        var children = [
+            this.getUserNode(),
+        ];
+        return { label, url, children };
+    },
+
+    getUserNode: function() {
+        var t = this.props.locale.translate;
+        var user = this.state.user;
+        var label = user.details.name || user.username || t('nav-user-name-pending');
+        var userId = this.props.route.parameters.userId;
+        var url = require('pages/user-summary-page').getUrl({ userId });
+        return { label, url };
+    },
+
+    getRoleListNode: function() {
+        var t = this.props.locale.translate;
+        var url = require('pages/role-list-page').getUrl();
+        var label = t('nav-roles');
+        var children = [
+            this.getRoleNode(),
+        ];
+        return { label, url, children };
+    },
+
+    getRoleNode: function() {
+        var t = this.props.locale.translate;
+        var p = this.props.locale.pick;
+        var role = this.state.role;
+        var label = p(role.details.tile) || role.name || t('nav-role-name-pending');
+        var roleId = this.props.route.parameters.roleId;
+        var url = require('pages/role-summary-page').getUrl({ roleId });
+        return { label, url };
+    },
+
+    getServerListNode: function() {
+        var t = this.props.locale.translate;
+        var url = require('pages/server-list-page').getUrl();
+        var label = t('nav-servers');
+        var children = [
+            this.getServerNode(),
+        ];
+        return { label, url, children };
+    },
+
+    getServerNode: function() {
+        var t = this.props.locale.translate;
+        var p = this.props.locale.pick;
+        var server = this.state.server;
+        var label = p(server.details.tile) || server.name || t('nav-server-name-pending');
+        var serverId = this.props.route.parameters.serverId;
+        var url = require('pages/server-summary-page').getUrl({ serverId });
+        return { label, url };
+    },
+
+    getSettingsNode: function() {
+        var t = this.props.locale.translate;
+        var url = require('pages/settings-page').getUrl();
+        var label = t('nav-servers');
+        return { label, url };
     },
 
     repositionArrow: function() {
