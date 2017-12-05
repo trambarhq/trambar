b66ca15a61190f1275402b37ee6b9fac463aca40
Chung Leong <chernyshevsky@hotmail.com>
2017-12-05 21:28:52 +0100

    Added favicon to Admin Console (issue #94).

--- admin/webpack.config.js
+++ admin/webpack.config.js
@@ -101,6 +101,10 @@ module.exports = {
                     ]
                 }
             },
+            {
+                test: /\.(jpeg|jpg|png|gif)$/,
+                loader: 'file-loader',
+            },
         ]
     },
     plugins: [
