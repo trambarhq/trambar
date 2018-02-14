428e8cc7861f2be568a463f887bff9b26d52ff26
Chung Leong <chernyshevsky@hotmail.com>
2018-02-14 23:24:06 +0100

    Implemented handling of limits on client-side (issue #246).

--- common/src/data/local-search.js
+++ common/src/data/local-search.js
@@ -2,30 +2,49 @@ var _ = require('lodash');
 
 module.exports = {
     match,
-}
+    limit,
+};
 
+/**
+ * Check if an object matches the provided criteria
+ *
+ * Need to keep functionality in-sync with backend
+ *
+ * @param  {String} table
+ * @param  {Object} object
+ * @param  {Object} criteria
+ *
+ * @return {Boolean}
+ */
 function match(table, object, criteria) {
     var matching = true;
     for (var name in criteria) {
         var desiredValue = criteria[name];
+        if (desiredValue === undefined) {
+            continue;
+        }
         if (object.hasOwnProperty(name)) {
             var actualValue = object[name];
             if (desiredValue instanceof Array) {
                 if (actualValue instanceof Array) {
+                    // array value matches an array when there's overlapping
+                    // between the two
                     if (_.intersection(desiredValue, actualValue).length === 0) {
                         matching = false;
-                        break;
                     }
                 } else {
+                    // array value matches a scalar when the former contains
+                    // the latter
                     if (!_.includes(desiredValue, actualValue)) {
                         matching = false;
-                        break;
                     }
                 }
             } else if (actualValue !== desiredValue) {
                 if (typeof(actualValue) === 'object' && typeof(desiredValue) === 'object') {
+                    // objects requires exact match
+                    // (NOTE: _.isMatch() might more make sense here)
                     if (!_.isEqual(actualValue, desiredValue)) {
-                        return false;
+                        matching = false;
                     }
                 } else {
                     matching = false;
@@ -47,8 +66,14 @@ function match(table, object, criteria) {
                     }
                     break;
                 case 'exclude':
-                    if (_.includes(desiredValue, object.id)) {
-                        matching = false;
+                    if (desiredValue instanceof Array) {
+                        if (_.includes(desiredValue, object.id)) {
+                            matching = false;
+                        }
+                    } else {
+                        if (desiredValue === object.id) {
+                            matching = false;
+                        }
                     }
                     break;
                 case 'newer_than':
@@ -62,13 +87,39 @@ function match(table, object, criteria) {
                     }
                     break;
                 case 'search':
-                    // never match against cached records
+                    // TODO: check usage to see if this is sensible
                     matching = false;
                     break;
                 default:
                     // assume it matches
             }
         }
+        if (!matching) {
+            break;
+        }
     }
     return matching;
+}
+
+function limit(table, objects, criteria) {
+    if (criteria) {
+        if (criteria.limit) {
+            // apply limit--trimming off objects with smaller ids
+            if (objects.length > criteria.limit) {
+                objects.splice(0, objects.length - criteria.limit);
+            }
+        }
+        if (criteria.per_user_limit) {
+            var objectsByUser = _.groupBy(objects, 'user_id');
+            _.each(objectsByUser, (list) => {
+                if (list.length > criteria.per_user_limit) {
+                    var count = list.length - criteria.per_user_limit;
+                    for (var i = 0; i < list.length; i++) {
+                        var index = objects.indexOf(list[0]);
+                        objects.splice(index, 1);
+                    }
+                }
+            });
+        }
+    }
 }
