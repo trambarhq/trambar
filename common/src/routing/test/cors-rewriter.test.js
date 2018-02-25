3f3c6b6df7a7d53b0e3482345d5590423bebcfc5
Chung Leong <chernyshevsky@hotmail.com>
2018-02-25 22:54:11 +0100

    Added test script for CORSRewriter (issue #283).

---
+++ common/src/routing/test/cors-rewriter.test.js
@@ -0,0 +1,53 @@
+var _ = require('lodash');
+var Chai = require('chai'), expect = Chai.expect;
+
+var CORSRewriter = require('routing/cors-rewriter');
+
+describe('CORSRewriter', function() {
+    describe('#extract', function() {
+        it('should extract an address from the path part of a URL', function() {
+            var urlParts = {
+                path: '/https/death-star.ge/somewhere/info'
+            };
+            var params = {};
+            CORSRewriter.extract(urlParts, params);
+            expect(urlParts.path).to.equal('/somewhere/info');
+            expect(params.address).to.equal('https://death-star.ge');
+            expect(params.cors).to.be.true;
+        })
+        it('should add address of document to parameters when there is no CORS host', function() {
+            var urlParts = {
+                path: '/somewhere/info'
+            };
+            var params = {};
+            CORSRewriter.extract(urlParts, params);
+            expect(urlParts.path).to.equal('/somewhere/info');
+            expect(params.address).to.equal(`${location.protocol}//${location.host}`);
+            expect(params.cors).to.be.false;
+        })
+    })
+    describe('#insert', function() {
+        it('should insert CORS host address into path', function() {
+            var urlParts = {
+                path: '/somewhere/info'
+            };
+            var params = {
+                cors: true,
+                address: 'https://death-star.ge'
+            };
+            CORSRewriter.insert(urlParts, params);
+            expect(urlParts.path).to.equal('/https/death-star.ge/somewhere/info');
+        })
+        it('should do nothing when CORS is not employed', function() {
+            var urlParts = {
+                path: '/somewhere/info'
+            };
+            var params = {
+                cors: false,
+                address: `${location.protocol}//${location.host}`
+            };
+            CORSRewriter.insert(urlParts, params);
+            expect(urlParts.path).to.equal('/somewhere/info');
+        })
+    })
+})
