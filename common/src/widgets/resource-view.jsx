79a96db58eaf4c74cff39804e5f558fc0e05792f
Chung Leong <chernyshevsky@hotmail.com>
2018-03-04 15:03:42 +0100

    Implemented preloading of just uploaded images (issue #290).

--- common/src/widgets/resource-view.jsx
+++ common/src/widgets/resource-view.jsx
@@ -1,95 +1,240 @@
 var _ = require('lodash');
 var React = require('react'), PropTypes = React.PropTypes;
-
-module.exports = ResourceView;
+var MediaLoader = require('media/media-loader');
+var ImageCropping = require('media/image-cropping');
 
 var Payload = require('transport/payload');
 var Theme = require('theme/theme');
 var BitmapView = require('media/bitmap-view');
 var VectorView = require('media/vector-view');
 
-function ResourceView(props) {
-    var url;
-    if (props.resource) {
-        var params = {};
-        if (props.animation && props.resource.format === 'gif') {
-            // use the original file when it's a gif and we wish to show animation
-            params.original = true;
+module.exports = React.createClass({
+    displayName: 'ResourceView',
+    propTypes: {
+        clip: PropTypes.bool,
+        animation: PropTypes.bool,
+        resource: PropTypes.object,
+        url: PropTypes.string,
+        width: PropTypes.number,
+        height: PropTypes.number,
+        theme: PropTypes.instanceOf(Theme),
+    },
+
+    /**
+     * Return default props
+     *
+     * @return {Object}
+     */
+    getDefaultProps: function() {
+        return {
+            clip: true,
+        };
+    },
+
+    /**
+     * Return initial state of component
+     *
+     * @return {Object}
+     */
+    getInitialState: function() {
+        return {
+            isWaitingForImage: false
+        };
+    },
+
+    /**
+     * Return URL of image
+     *
+     * @param  {Object} props
+     *
+     * @return {String|undefined}
+     */
+    getURL: function(props) {
+        props = props || this.props;
+        if (props.resource) {
+            var params = {};
+            if (props.animation && props.resource.format === 'gif') {
+                // use the original file when it's a gif and we wish to show animation
+                params.original = true;
+            } else {
+                if (!props.clip) {
+                    // don't apply clip rectangle
+                    params.clip = null;
+                }
+                if (props.width) {
+                    // resize source image
+                    params.width = props.width;
+                }
+                if (props.height) {
+                    // ditto
+                    params.height = props.height;
+                }
+            }
+            return props.theme.getImageURL(props.resource, params);
         } else {
-            if (!props.clip) {
-                // don't apply clip rectangle
-                params.clip = null;
-            }
-            if (props.width) {
-                // resize source image
-                params.width = props.width;
-            }
-            if (props.height) {
-                // ditto
-                params.height = props.height;
-            }
+            return props.url;
         }
-        url = props.theme.getImageURL(props.resource, params);
-    } else {
-        url = props.url;
-    }
-    var className = props.className;
-    var handlers = _.pick(props, 'onLoad', 'onError');
-    if (!url) {
-        return props.children || null;
-    }
-    if (/^https?:/.test(url)) {
+    },
+
+    /**
+     * Return image dimensions
+     *
+     * @param  {Object} props
+     *
+     * @return {Object}
+     */
+    getDimensions: function(props) {
+        props = props || this.props;
         var width = props.width;
         var height = props.height;
-        if (width === undefined || height === undefined) {
-            if (props.resource) {
-                var dims = props.theme.getImageDimensions(props.resource);
-                if (width) {
-                    height = Math.round(width * dims.height / dims.width);
-                } else if (height) {
-                    width = Math.round(height * dims.width / dims.height);
-                } else {
-                    width = dims.width;
-                    height = dims.height;
+        if (props.resource) {
+            var dims = props.theme.getImageDimensions(props.resource);
+            if (width) {
+                height = Math.round(width * dims.height / dims.width);
+            } else if (height) {
+                width = Math.round(height * dims.width / dims.height);
+            } else {
+                width = dims.width;
+                height = dims.height;
+            }
+        }
+        return { width, height };
+    },
+
+    /**
+     * Return clipping rectangle
+     *
+     * @param  {Object} props
+     *
+     * @return {Object|undefined}
+     */
+    getClippingRect: function(props) {
+        props = props || this.props;
+        if (!props.clip) {
+            return;
+        }
+        var clip;
+        if (props.resource) {
+            clip = props.resource.clip;
+            if (!clip) {
+                // apply default
+                clip = ImageCropping.default(props.resource.width, props.resource.height);
+            }
+        } else {
+            var url = props.url;
+            if (url) {
+                // extract from URL hash
+                var hashIndex = url.indexOf('#');
+                if (hashIndex !== -1) {
+                    var hash = parseQueryString(url.substr(hashIndex + 1));
+                    url = url.substr(0, hashIndex);
+                    if (hash.left && hash.top && hash.width && hash.height) {
+                        clip = {
+                            left: parseInt(hash.left),
+                            top: parseInt(hash.top),
+                            width: parseInt(hash.width),
+                            height: parseInt(hash.height),
+                        };
+                    }
                 }
             }
         }
-        return <img className={className} src={url} width={width} height={height} {...handlers} />;
-    } else {
-        var hashIndex = url.indexOf('#');
-        var clip, format;
-        var hash = parseQueryString(url.substr(hashIndex + 1));
-        url = url.substr(0, hashIndex);
-        if (hash.left && hash.top && hash.width && hash.height) {
-            clip = {
-                left: parseInt(hash.left),
-                top: parseInt(hash.top),
-                width: parseInt(hash.width),
-                height: parseInt(hash.height),
-            };
-        }
-        var View = BitmapView;
-        if (hash.format === 'svg') {
-            View = VectorView;
-        }
-        return <View className={className} url={url} clippingRect={clip} {...handlers} />
-    }
-}
+        return clip;
+    },
 
-ResourceView.propTypes = {
-    clip: PropTypes.bool,
-    animation: PropTypes.bool,
-    resource: PropTypes.object,
-    url: PropTypes.string,
-    width: PropTypes.number,
-    height: PropTypes.number,
-    theme: PropTypes.instanceOf(Theme),
-    onLoad: PropTypes.func,
-    onError: PropTypes.func,
-};
-ResourceView.defaultProps = {
-    clip: true,
-};
+    /**
+     * Return true if file is SVG
+     *
+     * @param  {Object} props
+     *
+     * @return {Boolean}
+     */
+    isVector: function(props) {
+        props = props || this.props;
+        if (!props.clip) {
+            return;
+        }
+        var format;
+        if (props.resource) {
+            format = props.resource.format;
+        } else {
+            var url = props.url;
+            if (url) {
+                var hashIndex = url.indexOf('#');
+                if (hashIndex !== -1) {
+                    var hash = parseQueryString(url.substr(hashIndex + 1));
+                    format = hash.format;
+                }
+            }
+        }
+        return (format === 'svg');
+    },
+
+    /**
+     * Check if we're switching for showing blob to showing remote file
+     *
+     * @param  {Object} nextProps
+     */
+    componentWillReceiveProps: function(nextProps) {
+        if (this.props.resource !== nextProps.resource) {
+            var urlBefore = this.getURL();
+            var urlAfter = this.getURL(nextProps);
+            if (!remote.test(urlBefore) && remote.test(urlAfter)) {
+                //
+                this.setState({ isWaitingForImage: true });
+                MediaLoader.loadImage(urlAfter).catch((err) => {
+                }).then(() => {
+                    this.setState({ isWaitingForImage: false });
+                });
+            }
+        }
+    },
+
+    /**
+     * Suppress update while remote image is loading
+     *
+     * @param  {Object} nextProps
+     * @param  {Object} nextState
+     *
+     * @return {Boolean}
+     */
+    shouldComponentUpdate: function(nextProps, nextState) {
+        if (nextState.isWaitingForImage) {
+            return false;
+        }
+        return true;
+    },
+
+    /**
+     * Render component
+     *
+     * @return {ReactElement}
+     */
+    render: function() {
+        var url = this.getURL();
+        if (!url) {
+            return this.props.children || null;
+        }
+        var props = _.omit(this.props, 'clip', 'animation', 'resource', 'url', 'width', 'height', 'theme');
+        if (remote.test(url)) {
+            var dims = this.getDimensions();
+            props.src = url;
+            props.width = dims.width;
+            props.height = dims.height;
+            return <img {...props} />;
+        } else {
+            props.url = url;
+            props.clippingRect = this.getClippingRect();
+            if (this.isVector()) {
+                return <VectorView {...props} />;
+            } else {
+                return <BitmapView {...props} />;
+            }
+        }
+    },
+});
+
+var remote = /^https?:/;
 
 function parseQueryString(queryString) {
     var values = {};
