cdb2a8504577ad133a540ef189047490c8adf7c1
Chung Leong <chernyshevsky@hotmail.com>
2018-03-31 19:42:22 +0200

    Reworked FocusManager (issue #373).

--- common/src/utils/focus-manager.js
+++ common/src/utils/focus-manager.js
@@ -7,32 +7,34 @@ module.exports = {
 };
 
 var entries = [];
+var requests = [];
 
 function register(component, props) {
     entries.unshift({ component, props });
+
+    // see if a request for focus has been made
+    requests = _.filter(requests, (request) => {
+        if (_.isMatch(props, request)) {
+            component.focus();
+            return false;
+        } else {
+            return true;
+        }
+    });
 }
 
 function unregister(component) {
-    _.pull(entries, { component });
+    entries = _.filter(entries, (entry) => {
+        return (entry.component !== component);
+    });
 }
 
 function focus(props) {
-    //console.log('focus', props);
-    var attempts = 0;
-    var interval = setInterval(() => {
-        var entry = _.find(entries, { props });
-        if (entry) {
-            try {
-                entry.component.focus();
-            } catch (err) {
-                console.error(err);
-            }
-            clearInterval(interval);
-        } else {
-            attempts++;
-            if (attempts > 10) {
-                clearInterval(interval);
-            }
-        }
-    }, 10);
+    var entry = _.find(entries, { props });
+    if (entry) {
+        entry.component.focus()
+    } else {
+        // store the request and set focus when component registers itself
+        requests.push(props);
+    }
 }
