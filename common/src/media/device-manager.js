b21e0bd95f9bb2f508066c0c0933f1d3888b305e
Chung Leong <chernyshevsky@hotmail.com>
2018-04-19 23:13:48 +0200

    Made MediaManager.hasDevice() return true when detection isn't possible (issue #485)

--- common/src/media/device-manager.js
+++ common/src/media/device-manager.js
@@ -10,6 +10,13 @@ module.exports = {
 
 var availableDevices = [];
 
+/**
+ * Return a list of video or audio capture devices
+ *
+ * @param  {String} kind
+ *
+ * @return {Array<Object>}
+ */
 function getDevices(kind) {
     if (kind) {
         return _.filter(availableDevices, { kind })
@@ -18,25 +25,52 @@ function getDevices(kind) {
     }
 }
 
+/**
+ * Return true if a video or audio capture is available
+ *
+ * @param  {String}  kind
+ *
+ * @return {Boolean}
+ */
 function hasDevice(kind) {
+    if (!navigator.mediaDevices) {
+        // just assume a camera and microphone are avilable
+        // since this should only happen on the an iPhone or a Mac
+        return true;
+    }
     var devices = getDevices(kind);
     return devices.length > 0;
 }
 
 var eventListeners = [];
 
+/**
+ * Add event listener
+ *
+ * @param {String} eventType
+ * @param {Function} f
+ */
 function addEventListener(eventType, f) {
     if (eventType === 'change') {
         eventListeners.push(f);
     }
 }
 
+/**
+ * Remove event listener
+ *
+ * @param {String} eventType
+ * @param {Function} f
+ */
 function removeEventListener(eventType, f) {
     if (eventType === 'change') {
         eventListeners = _.pull(eventListeners, f);
     }
 }
 
+/**
+ * Scan system for media devices then trigger change event
+ */
 function scan() {
     return Promise.try(() => {
         if (!navigator.mediaDevices) {
