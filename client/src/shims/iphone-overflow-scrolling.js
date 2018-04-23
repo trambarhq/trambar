b788708243ae8d5e4760ca416fc58defa5619f30
Chung Leong <chernyshevsky@hotmail.com>
2018-04-23 22:03:52 +0200

    Implemented different fix for iPhone rendering bug (issue #498).

--- client/src/shims/iphone-overflow-scrolling.js
+++ client/src/shims/iphone-overflow-scrolling.js
@@ -1,26 +1,4 @@
-// install Safari specific shim
-var bodyStyle = getComputedStyle(document.body);
-if (bodyStyle.WebkitOverflowScrolling !== undefined) {
-    // need to disable momentum scrolling when textarea has focus
-    // to prevent rendering glitch
-    var isFromFormField = (evt) => {
-        switch (evt.target.tagName) {
-            case 'TEXTAREA':
-            case 'INPUT':
-            case 'BUTTON':
-                return true;
-            default:
-                return false;
-        }
-    };
-    window.addEventListener('focusin', (evt) => {
-        if (isFromFormField(evt)) {
-            document.body.style.WebkitOverflowScrolling = 'auto';
-        }
-    });
-    window.addEventListener('focusout', (evt) => {
-        if (isFromFormField(evt)) {
-            document.body.style.WebkitOverflowScrolling = '';
-        }
-    });
+// install iOS Safari specific CSS
+if (/iPad|iPhone|iPod/.test(navigator.platform)) {
+    require('./iphone-overflow-scrolling.scss');
 }
