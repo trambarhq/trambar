75269127cd96f217977d61f9d0f4d2275fc6d7bd
Chung Leong <chernyshevsky@hotmail.com>
2018-05-28 11:55:06 +0200

    Removed dependency on lodash from shim.

--- client/src/shims/iphone-image-reload.js
+++ client/src/shims/iphone-image-reload.js
@@ -1,14 +1,13 @@
 if (process.env.PLATFORM === 'cordova') {
-    var _ = require('lodash');
-
     if (/iPad|iPhone|iPod/.test(navigator.platform)) {
         document.addEventListener('resume', () => {
             var images = document.getElementsByTagName('IMG');
-            _.each(images, (image) => {
-                if (image.naturalWidth === 0 && image.src) {
+            for (var i = 0; i < images.length; i++) {
+                var image = images[i];
+                if (image.naturalWidth === 0 && !!image.src) {
                     image.src = image.src;
                 }
-            });
+            }
         });
     }
 }
