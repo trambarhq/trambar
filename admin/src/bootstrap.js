eea8ad1529e802542ab221dff852cf7cbbd04e63
Chung Leong <chernyshevsky@hotmail.com>
2018-07-05 17:01:06 +0200

    Added progress bar to admin console splash screen (issue #641).

--- admin/src/bootstrap.js
+++ admin/src/bootstrap.js
@@ -6,6 +6,26 @@ function initialize(evt) {
         throw new Error('Unable to find app element in DOM');
     }
 
+    var progress;
+    var progressBar = document.getElementById('bootstrap-progress-bar');
+    var progressBarFilled = document.getElementById('bootstrap-progress-bar-filled');
+    if (progressBar && progressBarFilled) {
+        var finished = false;
+        setTimeout(() => {
+            // don't show progress bar when loading finishes quickly
+            if (!finished) {
+                progressBar.className = 'show';
+            }
+        }, 500);
+        progress = (loaded, total) => {
+            if (loaded === total) {
+                progressBar.className = '';
+                finished = true;
+            }
+            progressBarFilled.style.width = Math.round(loaded / total * 100) + '%';
+        };
+    }
+
     // load application code and support libraries
     var BootstrapLoader = require('utils/bootstrap-loader');
     var importFuncs = {};
@@ -14,7 +34,7 @@ function initialize(evt) {
         importFuncs[key] = libraries[key];
     }
     importFuncs['app'] = () => import('application' /* webpackChunkName: "app" */);
-    BootstrapLoader.load(importFuncs).then((modules) => {
+    BootstrapLoader.load(importFuncs, progress).then((modules) => {
         var Application = modules['app'];
         var React = modules['react'];
         var ReactDOM = modules['react-dom'];
