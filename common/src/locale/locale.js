3ab1e8ab479c91baede80c49393b666ff6d24c2e
Chung Leong <chernyshevsky@hotmail.com>
2018-09-10 04:28:50 +0200

    Reworked locale proxy.

--- common/src/locale/locale.js
+++ common/src/locale/locale.js
@@ -1,38 +1,35 @@
-module.exports = Locale;
+class Locale {
+    constructor(localeManager) {
+        this.localeManager = localeManager;
+        this.directory = localeManager.directory;
+        this.localeCode = localeManager.localeCode;
+        this.languageCode = localeManager.languageCode;
+        this.countryCode = localeManager.countryCode;
 
-function Locale(localeManager) {
-    this.localeManager = localeManager;
-    this.directory = this.localeManager.getDirectory();
-    this.localeCode = this.localeManager.getLocaleCode();
-    var parts = _.split(this.localeCode, '-');
-    this.languageCode = parts[0];
-    this.countryCode = parts[1];
+        // bound shorthand functions
+        this.t = this.translate.bind(this)
+        this.p = this.pick.bind(this)
+        this.g = this.genderize.bind(this)
+    }
 
-    // bind the functions to this so we can use short aliases
-    this.translate = this.translate.bind(this);
-    this.pick = this.pick.bind(this);
-    this.name = this.name.bind(this);
-    this.change = this.change.bind(this);
+    translate(phrase) {
+        this.localeManager.translate(phrase);
+    }
+
+    pick(versions, overridingLanguageCode) {
+        this.localeManager.pick(versions, overridingLanguageCode);
+    }
+
+    genderize(name, gender) {
+        this.localeManager.genderize(name, gender);
+    }
+
+    change(localeCode) {
+        this.localeManager.change(localeCode);
+    }
 }
 
-Locale.prototype.translate = function(phrase) {
-    if (phrase instanceof Object) {
-        return this.localeManager.pick(phrase);
-    }
-    return this.localeManager.translate.apply(this.localeManager, arguments);
-};
-
-Locale.prototype.pick = function(phraseVersions, overrideLanguageCode) {
-    return this.localeManager.pick(phraseVersions, overrideLanguageCode);
-};
-
-Locale.prototype.name = function(nameVersions, gender, overrideLanguageCode) {
-    var name = this.localeManager.pick(nameVersions, overrideLanguageCode);
-    var obj = new String(name || '');
-    obj.gender = gender;
-    return obj;
-};
-
-Locale.prototype.change = function(localeCode) {
-    return this.localeManager.change(localeCode);
+export {
+    Locale as default,
+    Locale,
 };
