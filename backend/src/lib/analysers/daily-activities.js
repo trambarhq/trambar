2efd1b46eec31d0745daea8344794df0b269d249
Chung Leong <chernyshevsky@hotmail.com>
2018-12-17 12:21:23 +0100

    Refactored statistics analysers.

--- backend/src/lib/analysers/daily-activities.js
+++ backend/src/lib/analysers/daily-activities.js
@@ -3,75 +3,74 @@ import Moment from 'moment';
 
 import Story from 'accessors/story';
 
-const DailyActivities = {
-    type: 'daily-activities',
-    // tables from which the stats are derived
-    sourceTables: [ 'story' ],
-    // filters and the columns they act on--determine which objects are
-    // included in the statistics;
-    filteredColumns: {
-        story: {
-            user_ids: 'user_ids',
-            role_ids: 'role_ids',
-            time_range: 'ptime',
-            external_object: 'external',
-            public: 'public',
-        },
-    },
-    // additional criteria that objects must also meet to be included
-    fixedFilters: {
-        story: {
-            deleted: false,
-            published: true,
-        }
-    },
-    // columns in the table(s) that affects the results (columns used by the
-    // filters would, of course, also impact the results)
-    depedentColumns: {
-        story: [
-            'type',
-            'ptime',
-            'tags',
-        ],
-    },
+class DailyActivities {
+    constructor() {
+        this.type = 'daily-activities';
+        // tables from which the stats are derived
+        this.sourceTables = [ 'story' ];
+        // filters and the columns they act on--determine which objects are
+        // included in the statistics;
+        this.filteredColumns = {
+            story: {
+                user_ids: 'user_ids',
+                role_ids: 'role_ids',
+                time_range: 'ptime',
+                external_object: 'external',
+                public: 'public',
+            },
+        };
+        // additional criteria that objects must also meet to be included
+        this.fixedFilters = {
+            story: {
+                deleted: false,
+                published: true,
+            }
+        };
+        // columns in the table(s) that affects the results (columns used by the
+        // filters would, of course, also impact the results)
+        this.depedentColumns = {
+            story: [
+                'type',
+                'ptime',
+                'tags',
+            ],
+        };
+    }
 
-    generate: function(db, schema, filters) {
+    async generate(db, schema, filters) {
         // apply fixed filters
-        var criteria = _.clone(this.fixedFilters.story);
+        let criteria = _.clone(this.fixedFilters.story);
         // then apply per-row filters
         _.assign(criteria, _.omit(filters, 'timezone'));
 
         // load the stories
-        return Story.find(db, schema, criteria, 'type, tags, ptime').then((rows) => {
-            var tzOffset = _.get(filters, 'tz_offset', 0);
-            var activities = {};
-            var count = function(date, key) {
-                var counts = activities[date];
+        let rows = await Story.find(db, schema, criteria, 'type, tags, ptime');
+        let tzOffset = _.get(filters, 'tz_offset', 0);
+        let activities = {};
+        for (let row of rows) {
+            // get the date, taking into consideration the timezone requested
+            let date = Moment(row.ptime).utcOffset(tzOffset).format('YYYY-MM-DD');
+            // count both story types and tags
+            let keys = _.concat(row.type, row.tags);
+            for (let key of keys) {
+                let counts = activities[date];
                 if (!counts) {
                     counts = activities[date] = {};
                 }
                 // increment the count for key
                 counts[key] = (counts[key] || 0) + 1;
-            };
-            _.each(rows, (row) => {
-                // get the date, taking into consideration the timezone requested
-                var date = Moment(row.ptime).utcOffset(tzOffset).format('YYYY-MM-DD');
-                // count the story type
-                count(date, row.type);
-                // count the tags as well
-                _.each(row.tags, (tag) => {
-                    count(date, tag);
-                });
-            });
-            return {
-                details: activities,
-                sample_count: rows.length,
-            };
-        });
+            }
+        }
+        return {
+            details: activities,
+            sample_count: rows.length,
+        };
     }
 }
 
+const instance = new DailyActivities;
+
 export {
-    DailyActivities as default,
+    instance as default,
     DailyActivities,
 };
--- backend/src/lib/analysers/story-date-range.js
+++ backend/src/lib/analysers/story-date-range.js
@@ -1,50 +1,53 @@
 import _ from 'lodash';
 import Story from 'accessors/story';
 
-const StoryDateRange = {
-    type: 'story-date-range',
-    // tables from which the stats are derived
-    sourceTables: [ 'story' ],
-    filteredColumns: {
-        story: {
-            user_ids: 'user_ids',
-            role_ids: 'role_ids',
-            external_object: 'external',
-            public: 'public',
-        },
-    },
-    depedentColumns: {
-        story: [
-            'ptime',
-        ],
-    },
-    // additional criteria that objects must also meet to be included
-    fixedFilters: {
-        story: {
-            deleted: false,
-            published: true,
-        }
-    },
+class StoryDateRange {
+    constructor() {
+        this.type = 'story-date-range';
+        // tables from which the stats are derived
+        this.sourceTables = [ 'story' ];
+        this.filteredColumns = {
+            story: {
+                user_ids: 'user_ids',
+                role_ids: 'role_ids',
+                external_object: 'external',
+                public: 'public',
+            },
+        };
+        this.depedentColumns = {
+            story: [
+                'ptime',
+            ],
+        };
+        // additional criteria that objects must also meet to be included
+        this.fixedFilters = {
+            story: {
+                deleted: false,
+                published: true,
+            }
+        };
+    }
 
-    generate: function(db, schema, filters) {
+    async generate(db, schema, filters) {
         // apply fixed filters
-        var criteria = _.clone(this.fixedFilters.story);
+        let criteria = _.clone(this.fixedFilters.story);
         // then apply per-row filters
         _.assign(criteria, filters);
-        var columns = 'MIN(ptime), MAX(ptime), COUNT(ptime)';
-        return Story.findOne(db, schema, criteria, columns).then((row) => {
-            return {
-                details: {
-                    start_time: _.get(row, 'min', ''),
-                    end_time: _.get(row, 'max', ''),
-                },
-                sample_count: _.get(row, 'count', 0),
-            };
-        });
+        let columns = 'MIN(ptime), MAX(ptime), COUNT(ptime)';
+        let row = await Story.findOne(db, schema, criteria, columns);
+        return {
+            details: {
+                start_time: _.get(row, 'min', ''),
+                end_time: _.get(row, 'max', ''),
+            },
+            sample_count: _.get(row, 'count', 0),
+        };
     }
 }
 
+const instance = new StoryDateRange;
+
 export {
-    StoryDateRange as default,
+    instance as default,
     StoryDateRange,
 };
